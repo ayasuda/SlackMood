ed52da8b88738b101f5e23e16e0a20c8a7052d5a
Atsushi Yasuda <atsushi.yasuda.jp@gmail.com>
2015-08-18 19:56:40 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+SlackMood - Post What Listen on iTunes & AppleMusic to Slack
+==
+
+How to compile
+---
+
+setup cocoa pods
+
+> $ cd /path/to/SlackMood
+> $ pod install
+
+edit api key & channel
+
+> $ open SlackMood.xcworkspace
+
+edit SlackMood/AppDelegate.swift
+
+and build!!
---
+++ SlackMood/AppDelegate.swift
@@ -0,0 +1,46 @@
+import Cocoa
+import Alamofire
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    let api_url = "https://slack.com/api/chat.postMessage"
+    let api_channel = "#music" // channel to post
+    let api_token = "xoxp-your-api-token" // your slack token
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        NSDistributedNotificationCenter.defaultCenter().addObserver(
+            self, selector: "update:", name: "com.apple.iTunes.playerInfo", object: nil)
+    }
+    
+    func applicationWillTerminate(aNotification: NSNotification) {
+        NSDistributedNotificationCenter.defaultCenter().removeObserver(
+            self, name: "com.apple.iTunes.playerInfo", object: nil)
+    }
+    
+    
+    func update(notification: NSNotification?) {
+        if (notification?.userInfo?["Player State"] as? String ?? "none") != "Playing"
+        {
+            return
+        }
+        let name   = notification?.userInfo?["Name"] as? String ?? "none"
+        let artist = notification?.userInfo?["Artist"] as? String ?? "none"
+        let moodMessage = "listening... " + name + " - " + artist
+
+        var params: [String: AnyObject] = [
+            "channel" : api_channel,
+            "token" : api_token,
+            "as_user" : true,
+            "text" : moodMessage
+        ]
+        var payload: [String: AnyObject] = [
+            "payload" : params
+        ]
+        
+        Alamofire
+            .request(.POST, api_url, parameters: params, encoding: ParameterEncoding.URL, headers: nil)
+            .response { (request, response, data, error) -> Void in
+                println(response)
+        }
+    }
+}
