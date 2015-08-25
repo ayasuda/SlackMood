52308723bc14f3b3680ced1cfa722f02c2698283
SAWADA Tadashi <cesare@mayverse.jp>
2015-08-25 17:10:09 +0900

    Set the quit menu

--- SlackMood/AppDelegate.swift
+++ SlackMood/AppDelegate.swift
@@ -3,11 +3,15 @@ import Alamofire
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+    
     let api_url = "https://slack.com/api/chat.postMessage"
     let api_channel = "#music" // channel to post
     let api_token = "xoxp-your-api-token" // your slack token
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
+        setupMenu()
+        
         NSDistributedNotificationCenter.defaultCenter().addObserver(
             self, selector: "update:", name: "com.apple.iTunes.playerInfo", object: nil)
     }
@@ -39,5 +43,21 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             .response { (request, response, data, error) -> Void in
                 println(response)
         }
+    }
+    
+    func setupMenu() {
+        let menu = NSMenu()
+        self.statusItem.title = "SlackMood"
+        self.statusItem.highlightMode = true
+        self.statusItem.menu = menu
+        
+        let menuItem = NSMenuItem()
+        menuItem.title = "Quit"
+        menuItem.action = Selector("quit:")
+        menu.addItem(menuItem)
+    }
+    
+    @IBAction func quit(sender: NSButton) {
+        NSApplication.sharedApplication().terminate(self)
     }
 }
