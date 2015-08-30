b82ba0eecf194b8799ed0ce4c8d3591883131ee0
SAWADA Tadashi <cesare@mayverse.jp>
2015-08-31 00:21:13 +0900

    Remove unused variables and methods

--- SlackMood/AppDelegate.swift
+++ SlackMood/AppDelegate.swift
@@ -3,10 +3,6 @@ import Alamofire
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    let api_url = "https://slack.com/api/chat.postMessage"
-    let api_channel = "#music" // channel to post
-    let api_token = "xoxp-your-api-token" // your slack token
-    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         startServices()
     }
@@ -23,28 +19,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     private func stopServices() {
         PlayerListeningService.sharedService().stop()
         SlackPostingService.sharedService().stop()
-    }
-    
-    func update(notification: NSNotification?) {
-        if (notification?.userInfo?["Player State"] as? String ?? "none") != "Playing"
-        {
-            return
-        }
-        let name   = notification?.userInfo?["Name"] as? String ?? "none"
-        let artist = notification?.userInfo?["Artist"] as? String ?? "none"
-        let moodMessage = "listening... " + name + " - " + artist
-
-        var params: [String: AnyObject] = [
-            "channel" : api_channel,
-            "token" : api_token,
-            "as_user" : true,
-            "text" : moodMessage
-        ]
-        
-        Alamofire
-            .request(.POST, api_url, parameters: params, encoding: ParameterEncoding.URL, headers: nil)
-            .response { (request, response, data, error) -> Void in
-                println(response)
-        }
     }
 }
